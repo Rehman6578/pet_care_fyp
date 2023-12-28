c65df8c2b588a8f4e20d3eddc01aa3526aaee269
Abdul Salam <salambangash16@gmail.com>
2023-12-28 22:02:14 +0500

    done 10:00

--- lib/views/Dashboard/ProfileScreens/PetServices/GroomingServices.dart
+++ lib/views/Dashboard/ProfileScreens/PetServices/GroomingServices.dart
@@ -1,7 +1,6 @@
 
 
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
@@ -11,6 +10,7 @@ import 'package:pet_care_fyp/const/images.dart';
 import 'package:pet_care_fyp/controllers/Pets_Services/PetController.dart';
 
 import '../../../../controllers/Image_Controller/ImageController.dart';
+import 'BoardingSerivce.dart';
 
 class AddGroomingService extends StatefulWidget {
   @override
@@ -34,11 +34,17 @@ class _AddGroomingServiceState extends State<AddGroomingService> {
   final TextEditingController _PetGroomingController = TextEditingController();
   String? selectedOption;
 
+  String? profileImg;
+  String? img1;
+  String? img2;
+  String? img3;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Add Grooming Service'),
+        title: const Text('Add Grooming Service',style: TextStyle(fontWeight: FontWeight.bold),),
+        centerTitle: true,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -48,6 +54,244 @@ class _AddGroomingServiceState extends State<AddGroomingService> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+
+                const Padding(
+                  padding: EdgeInsets.only(left: 8, right: 8),
+                  child: Text(
+                    'Upload image of your lovely house/compound where pets will be boarding.',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Center(
+                  child: Stack(
+                    children: [
+                      Card(
+                        elevation: 8.0,
+                        child: Container(
+                          height: Get.height * 0.25,
+                          width: Get.width,
+                          decoration: BoxDecoration(
+                            color: Colors.transparent,
+                            border: Border.all(
+                              color: Colors.grey,
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.circular(15.0),
+                            image: profileImg == null
+                                ? const DecorationImage(
+                                image: AssetImage(
+                                    'assets/images/trainingprofile.png'))
+                                : DecorationImage(
+                              image: FileImage(File(profileImg.toString())),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 10,
+                        right: 10,
+                        child: InkWell(
+                          onTap: () async {
+                            profileImg = await imagePickerController.GetImage();
+
+                            setState(() {});
+                          },
+                          child: Container(
+                            height: 40,
+                            width: 40,
+                            decoration: BoxDecoration(
+                              color: Colors.blue,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: const Icon(
+                              Icons.add,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  'Add lovely images of your pets',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: Get.height * 0.1,
+                  width: Get.width,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Stack(
+                        children: [
+                          Container(
+                            height: Get.height * 0.2,
+                            width: Get.width * 0.2,
+                            decoration: BoxDecoration(
+                              color: Colors.transparent,
+                              border: Border.all(
+                                color: Colors.grey,
+                                width: 1,
+                              ),
+                              borderRadius: BorderRadius.circular(10.0),
+                              image: img1 == null
+                                  ? const DecorationImage(
+                                  image: AssetImage(
+                                      'assets/images/petimage.png'))
+                                  : DecorationImage(
+                                image: FileImage(File(img1.toString())),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          // add shor addimage icon to add image of 20 * 20
+                          Positioned(
+                            bottom: 5,
+                            right: 5,
+                            child: InkWell(
+                              onTap: () async {
+                                img1 = await imagePickerController.GetImage();
+                                setState(() {});
+                              },
+                              child: Container(
+                                height: 20,
+                                width: 20,
+                                decoration: BoxDecoration(
+                                  color: Colors.blue,
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: const Icon(
+                                  Icons.add,
+                                  color: Colors.white,
+                                  size: 20,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Stack(
+                        children: [
+                          Container(
+                            height: Get.height * 0.2,
+                            width: Get.width * 0.2,
+                            decoration: BoxDecoration(
+                              color: Colors.transparent,
+                              border: Border.all(
+                                color: Colors.grey,
+                                width: 1,
+                              ),
+                              borderRadius: BorderRadius.circular(10.0),
+                              image: img2 == null
+                                  ? const DecorationImage(
+                                  image: AssetImage(
+                                      'assets/images/petimage.png'))
+                                  : DecorationImage(
+                                image: FileImage(File(img2.toString())),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            bottom: 5,
+                            right: 5,
+                            child: InkWell(
+                              onTap: () async {
+                                img2 = await imagePickerController.GetImage();
+                                setState(() {});
+                              },
+                              child: Container(
+                                height: 20,
+                                width: 20,
+                                decoration: BoxDecoration(
+                                  color: Colors.blue,
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: const Icon(
+                                  Icons.add,
+                                  color: Colors.white,
+                                  size: 20,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Stack(
+                        children: [
+                          Container(
+                            height: Get.height * 0.2,
+                            width: Get.width * 0.2,
+                            decoration: BoxDecoration(
+                              color: Colors.transparent,
+                              border: Border.all(
+                                color: Colors.grey,
+                                width: 1,
+                              ),
+                              borderRadius: BorderRadius.circular(10.0),
+                              image: img3 == null
+                                  ? const DecorationImage(
+                                  image: AssetImage(
+                                      'assets/images/petimage.png'))
+                                  : DecorationImage(
+                                image: FileImage(File(img3.toString())),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          Positioned(
+                            bottom: 5,
+                            right: 5,
+                            child: InkWell(
+                              onTap: () async {
+                                img3 = await imagePickerController.GetImage();
+                                setState(() {});
+                              },
+                              child: Container(
+                                height: 20,
+                                width: 20,
+                                decoration: BoxDecoration(
+                                  color: Colors.blue,
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                child: const Center(
+                                  child: Icon(
+                                    Icons.add,
+                                    color: Colors.white,
+                                    size: 20,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+
                 const SizedBox(height: 36.0),
                 const Text(
                   'Give a Name to your Service',
